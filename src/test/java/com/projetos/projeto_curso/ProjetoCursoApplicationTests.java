cbbf88657d47a7234eab7ca58bfab603747c3168
Marcos Vinicius <vsmarcos.15@gmail.com>
2023-10-03 18:57:22 -0300

    Projeto Criado

---
+++ src/test/java/com/projetos/projeto_curso/ProjetoCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetos.projeto_curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
