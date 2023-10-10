1612d94139a69f7461b54b2d3f2c4dadfee41ce1
Marcos Vinicius <vsmarcos.15@gmail.com>
2023-10-10 20:53:50 -0300

    Operações de inserção, deleção e atualização

--- src/main/java/com/projetos/projeto_curso/config/TestConfig.java
+++ src/main/java/com/projetos/projeto_curso/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.projetos.projeto_curso.entities.Category;
 import com.projetos.projeto_curso.entities.Order;
 import com.projetos.projeto_curso.entities.OrderItem;
+import com.projetos.projeto_curso.entities.Payment;
 import com.projetos.projeto_curso.entities.Product;
 import com.projetos.projeto_curso.entities.User;
 import com.projetos.projeto_curso.entities.enums.OrderStatus;
@@ -80,6 +81,11 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice()); 
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		OrderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T19:53:07Z"),o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 	
 	
--- src/main/java/com/projetos/projeto_curso/entities/Order.java
+++ src/main/java/com/projetos/projeto_curso/entities/Order.java
@@ -107,7 +107,13 @@ public class Order implements Serializable {
 		return items;
 	}
 	
-	
+	public Double getTotal() {
+		double sum = 0.0;
+		for(OrderItem x: items) {
+			sum = sum + x.getSubTotal();
+		}
+		return sum;
+	}
 
 	@Override
 	public int hashCode() {
