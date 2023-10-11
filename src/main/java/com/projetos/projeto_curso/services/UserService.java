b7cec475a2046b25515463baea7207d6cf8e6ec6
Marcos Vinicius <vsmarcos.15@gmail.com>
2023-10-10 21:15:51 -0300

    Exceções para update e delete

--- src/main/java/com/projetos/projeto_curso/services/UserService.java
+++ src/main/java/com/projetos/projeto_curso/services/UserService.java
@@ -4,10 +4,16 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.projetos.projeto_curso.entities.User;
 import com.projetos.projeto_curso.repositories.UserRepository;
+import com.projetos.projeto_curso.services.exceptions.DatabaseException;
+import com.projetos.projeto_curso.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +27,7 @@ public class UserService {
 	
 	public User findById(long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(()-> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
@@ -30,12 +36,23 @@ public class UserService {
 	
 	public void delete(Long id){
 		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id,User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity,obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 	
 	private void updateData(User entity, User obj) {
---
+++ src/main/java/com/projetos/projeto_curso/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.projetos.projeto_curso.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+	
+	private static final long serialVersionUID = 1L;
+
+	public ResourceNotFoundException(Object id) {
+		super("Recurso nao encontrado " + id);
+	}
+}
